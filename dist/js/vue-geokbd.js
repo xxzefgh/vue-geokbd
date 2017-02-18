0e0491e6527d275b7023984993b2ebb7d350ce5a
Mirza Brunjadze <brunjadzem@gmail.com>
2017-02-19 02:07:56 +0400

    Build

--- dist/js/vue-geokbd.js
+++ dist/js/vue-geokbd.js
@@ -54,288 +54,203 @@ return /******/ (function(modules) { // webpackBootstrap
 /* 0 */
 /***/ function(module, exports, __webpack_require__) {
 
-	'use strict';
+	"use strict";
 	
-	Object.defineProperty(exports, "__esModule", {
-	  value: true
-	});
-	
-	var _directive = __webpack_require__(1);
-	
-	var _directive2 = _interopRequireDefault(_directive);
-	
-	var _statusMsg = __webpack_require__(3);
-	
-	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-	
-	var install = function install(Vue) {
-	  var _ref = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : {},
-	      _ref$enabled = _ref.enabled,
-	      enabled = _ref$enabled === undefined ? false : _ref$enabled,
-	      _ref$hotkey = _ref.hotkey,
-	      hotkey = _ref$hotkey === undefined ? '~' : _ref$hotkey,
-	      _ref$statusMessage = _ref.statusMessage,
-	      statusMessage = _ref$statusMessage === undefined ? true : _ref$statusMessage;
-	
-	  var options = { enabled: enabled, hotkey: hotkey, statusMessage: statusMessage };
-	
-	  (0, _statusMsg.init)(options);
-	  Vue.directive('geokbd', (0, _directive2.default)(options));
+	var directive_1 = __webpack_require__(1);
+	var statusMsgEff_1 = __webpack_require__(3);
+	var install = function install(Vue, _a) {
+	    var _b = _a === void 0 ? {} : _a,
+	        _c = _b.hotkey,
+	        hotkey = _c === void 0 ? '~' : _c,
+	        _d = _b.enabled,
+	        enabled = _d === void 0 ? false : _d,
+	        _e = _b.statusMessage,
+	        statusMessage = _e === void 0 ? true : _e;
+	    var options = { hotkey: hotkey, enabled: enabled, statusMessage: statusMessage };
+	    statusMsgEff_1.default.init(options.hotkey, options.enabled);
+	    Vue.directive('geokbd', directive_1.default(options));
 	};
-	
-	exports.default = { install: install };
-	module.exports = exports['default'];
+	module.exports = { install: install };
 
 /***/ },
 /* 1 */
 /***/ function(module, exports, __webpack_require__) {
 
-	'use strict';
+	"use strict";
 	
-	Object.defineProperty(exports, "__esModule", {
-	  value: true
-	});
-	
-	var _keypress = __webpack_require__(2);
-	
-	var _keypress2 = _interopRequireDefault(_keypress);
-	
-	var _statusMsg = __webpack_require__(3);
-	
-	var _statusMsg2 = _interopRequireDefault(_statusMsg);
-	
-	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-	
+	var keypress_1 = __webpack_require__(2);
+	var statusMsgEff_1 = __webpack_require__(3);
+	Object.defineProperty(exports, "__esModule", { value: true });
 	exports.default = function (options) {
-	  return {
-	    bind: function bind(el, binding) {
-	      var _this = this;
-	
-	      // Vue 1.x support
-	      if (this && this.el) {
-	        el = this.el;
-	        binding = this;
-	      }
-	
-	      binding.keypressEvent = function (evt) {
-	        // Don't capture Ctrl/Meta keypress
-	        if (evt.metaKey || evt.ctrlKey) return;
-	
-	        // Check if hotkey was pressed
-	        if (options.hotkey === String.fromCharCode(evt.which)) {
-	          evt.preventDefault();
-	
-	          _this.toggleGlobalState();
-	          return;
+	    return {
+	        bind: function bind(el, binding) {
+	            // Vue 1.x support
+	            if (this && this.el) {
+	                el = this.el;
+	                binding = this;
+	            }
+	            binding.keypressEvent = function (evt) {
+	                // Don't capture Ctrl/Meta keypress
+	                if (evt.metaKey || evt.ctrlKey) {
+	                    return;
+	                }
+	                // Check if hotkey was pressed
+	                if (options.hotkey === String.fromCharCode(evt.which)) {
+	                    evt.preventDefault();
+	                    statusMsgEff_1.default.enabled(options.enabled = !options.enabled);
+	                } else {
+	                    // Check enabled state
+	                    if (options.enabled === true) {
+	                        keypress_1.default(evt);
+	                    }
+	                }
+	            };
+	            binding.focusEvent = function () {
+	                if (options.statusMessage) {
+	                    statusMsgEff_1.default.visibility(true);
+	                }
+	            };
+	            binding.blurEvent = function () {
+	                if (options.statusMessage) {
+	                    statusMsgEff_1.default.visibility(false);
+	                }
+	            };
+	            el.addEventListener('keypress', binding.keypressEvent);
+	            el.addEventListener('focus', binding.focusEvent);
+	            el.addEventListener('blur', binding.blurEvent);
+	        },
+	        unbind: function unbind(el, binding) {
+	            // Vue 1.x support
+	            if (this && this.el) {
+	                el = this.el;
+	                binding = this;
+	            }
+	            el.removeEventListener('keypress', binding.keypressEvent);
+	            el.removeEventListener('focus', binding.focusEvent);
+	            el.removeEventListener('blur', binding.blurEvent);
 	        }
-	
-	        // Check enabled state
-	        if (options.enabled === false) return;
-	
-	        (0, _keypress2.default)(evt);
-	      };
-	
-	      binding.focusEvent = function () {
-	        _statusMsg2.default.visibility(true, options.enabled, options.statusMessage);
-	      };
-	
-	      binding.blurEvent = function () {
-	        _statusMsg2.default.visibility(false, options.enabled, options.statusMessage);
-	      };
-	
-	      el.addEventListener('keypress', binding.keypressEvent);
-	      el.addEventListener('focus', binding.focusEvent);
-	      el.addEventListener('blur', binding.blurEvent);
-	    },
-	
-	    unbind: function unbind(el, binding) {
-	      // Vue 1.x support
-	      if (this && this.el) {
-	        el = this.el;
-	        binding = this;
-	      }
-	
-	      el.removeEventListener('keypress', binding.keypressEvent);
-	      el.removeEventListener('focus', binding.focusEvent);
-	      el.removeEventListener('blur', binding.blurEvent);
-	    },
-	
-	    toggleGlobalState: function toggleGlobalState() {
-	      options.enabled = !options.enabled;
-	      _statusMsg2.default.syncText(options.enabled);
-	    }
-	  };
+	    };
 	};
-	
-	module.exports = exports['default'];
 
 /***/ },
 /* 2 */
 /***/ function(module, exports) {
 
-	'use strict';
+	"use strict";
 	
-	Object.defineProperty(exports, "__esModule", {
-	  value: true
-	});
-	exports.default = handleKeypress;
-	function convertChar(chIndex) {
-	  var alphabet = 'abgdevzTiklmnopJrstufqRySCcZwWxjh';
-	  var ch = String.fromCharCode(chIndex);
-	
-	  if (alphabet.indexOf(ch) >= 0) {
-	    return alphabet.indexOf(ch) + 4304;
-	  }
-	
-	  return chIndex;
-	}
-	
-	function getInputSelection(el) {
-	  var start = 0,
-	      end = 0,
-	      normalizedValue,
-	      range,
-	      textInputRange,
-	      len,
-	      endRange;
-	
-	  if (typeof el.selectionStart === 'number' && typeof el.selectionEnd === 'number') {
-	    start = el.selectionStart;
-	    end = el.selectionEnd;
-	  } else {
-	    range = document.selection.createRange();
-	
-	    if (range && range.parentElement() === el) {
-	      len = el.value.length;
-	      normalizedValue = el.value.replace(/\r\n/g, '\n');
-	
-	      // Create a working TextRange that lives only in the input
-	      textInputRange = el.createTextRange();
-	      textInputRange.moveToBookmark(range.getBookmark());
-	
-	      // Check if the start and end of the selection are at the very end
-	      // of the input, since moveStart/moveEnd doesn't return what we want
-	      // in those cases
-	      endRange = el.createTextRange();
-	      endRange.collapse(false);
-	
-	      if (textInputRange.compareEndPoints('StartToEnd', endRange) > -1) {
-	        start = end = len;
-	      } else {
-	        start = -textInputRange.moveStart('character', -len);
-	        start += normalizedValue.slice(0, start).split('\n').length - 1;
-	
-	        if (textInputRange.compareEndPoints('EndToEnd', endRange) > -1) {
-	          end = len;
-	        } else {
-	          end = -textInputRange.moveEnd('character', -len);
-	          end += normalizedValue.slice(0, end).split('\n').length - 1;
-	        }
-	      }
+	var ALPHABET = 'abgdevzTiklmnopJrstufqRySCcZwWxjh';
+	var BASE_INDEX = 4304;
+	function convertChar(charIndex) {
+	    var char = String.fromCharCode(charIndex);
+	    if (ALPHABET.indexOf(char) >= 0) {
+	        return BASE_INDEX + ALPHABET.indexOf(char);
 	    }
-	  }
-	
-	  return {
-	    start: start,
-	    end: end
-	  };
+	    return charIndex;
 	}
-	
-	function offsetToRangeCharacterMove(el, offset) {
-	  return offset - (el.value.slice(0, offset).split('\r\n').length - 1);
+	function isSelectionSupported($input) {
+	    return typeof $input.selectionStart === 'number' && typeof $input.selectionEnd === 'number';
 	}
-	
-	function setInputSelection(el, startOffset, endOffset) {
-	  if (typeof el.selectionStart === 'number' && typeof el.selectionEnd === 'number') {
-	    el.selectionStart = startOffset;
-	    el.selectionEnd = endOffset;
-	  } else {
-	    var range = el.createTextRange();
-	    var startCharMove = offsetToRangeCharacterMove(el, startOffset);
-	
-	    range.collapse(true);
-	    if (startOffset === endOffset) {
-	      range.move('character', startCharMove);
-	    } else {
-	      range.moveEnd('character', offsetToRangeCharacterMove(el, endOffset));
-	      range.moveStart('character', startCharMove);
+	function getInputSelection($input) {
+	    var start = 0;
+	    var end = 0;
+	    if (isSelectionSupported($input)) {
+	        start = $input.selectionStart;
+	        end = $input.selectionEnd;
 	    }
-	    range.select();
-	  }
-	
-	  // A little 'hack' to keep natural browser behavior while typing
-	  el.blur();
-	  el.focus();
+	    return { start: start, end: end };
 	}
-	
+	function setInputSelection($input, offset) {
+	    if (isSelectionSupported($input)) {
+	        $input.selectionStart = offset.start;
+	        $input.selectionEnd = offset.end;
+	    }
+	    // A little 'hack' to keep natural browser behavior while typing
+	    $input.blur();
+	    $input.focus();
+	}
 	function handleKeypress(evt) {
-	  var el = evt.target;
-	  var which = evt.which;
-	  var to = convertChar(which);
-	
-	  // Don't take any actions if conversion didn't happen
-	  if (which === to) {
-	    return;
-	  }
-	  evt.preventDefault();
-	
-	  // Insert converted char
-	  var sel = getInputSelection(el);
-	  var val = el.value;
-	
-	  el.value = val.slice(0, sel.start) + String.fromCharCode(to) + val.slice(sel.end);
-	
-	  // Move caret to correct position
-	  setInputSelection(el, sel.start + 1, sel.start + 1);
+	    var $input = evt.target;
+	    var which = evt.which;
+	    var to = convertChar(which);
+	    // Don't take any action if conversion didn't happen
+	    if (which === to) {
+	        return;
+	    }
+	    evt.preventDefault();
+	    // Insert converted char
+	    var sel = getInputSelection($input);
+	    var val = $input.value;
+	    $input.value = val.slice(0, sel.start) + String.fromCharCode(to) + val.slice(sel.end);
+	    // Move caret to correct position
+	    setInputSelection($input, { start: sel.start + 1, end: sel.start + 1 });
 	}
-	module.exports = exports['default'];
+	Object.defineProperty(exports, "__esModule", { value: true });
+	exports.default = handleKeypress;
 
 /***/ },
 /* 3 */
+/***/ function(module, exports, __webpack_require__) {
+
+	"use strict";
+	
+	var statusMsgElement_1 = __webpack_require__(4);
+	var StatusMsgEff = function () {
+	    function StatusMsgEff() {}
+	    StatusMsgEff.init = function (hotkey, enabled) {
+	        statusMsgElement_1.default.create(hotkey);
+	        statusMsgElement_1.default.stateText(enabled);
+	    };
+	    StatusMsgEff.enabled = function (enabled) {
+	        statusMsgElement_1.default.stateText(enabled);
+	    };
+	    StatusMsgEff.visibility = function (visibility) {
+	        var _this = this;
+	        this.visibilityState = visibility = !!visibility;
+	        // Add little delay to keep consistent visibility state
+	        setTimeout(function () {
+	            if (_this.visibilityState === visibility) {
+	                statusMsgElement_1.default.isVisible(visibility);
+	            }
+	        }, 100);
+	    };
+	    return StatusMsgEff;
+	}();
+	StatusMsgEff.visibilityState = false;
+	Object.defineProperty(exports, "__esModule", { value: true });
+	exports.default = StatusMsgEff;
+
+/***/ },
+/* 4 */
 /***/ function(module, exports) {
 
-	'use strict';
+	"use strict";
 	
-	Object.defineProperty(exports, "__esModule", {
-	  value: true
-	});
-	var STATUS_MSG_TEXT_ON = 'ჩართულია';
-	var STATUS_MSG_TEXT_OFF = 'გამორთულია';
-	var STATUS_MSG_CLASS = 'vue-geokbd--statusMessage';
-	var STATUS_MSG_CLASS_VISIBLE = STATUS_MSG_CLASS + ' visible';
-	var STATUS_MSG_CLASS_HIDDEN = STATUS_MSG_CLASS + ' hidden';
-	
-	var element,
-	    showState = false;
-	
-	var init = exports.init = function init(options) {
-	  element = document.createElement('div');
-	  element.className = STATUS_MSG_CLASS;
-	  element.innerHTML = '\n  <div class="vue-geokbd--statusMessage-text">\n    \u10E5\u10D0\u10E0\u10D7\u10E3\u10DA\u10D8 \u10D9\u10DA\u10D0\u10D5\u10D8\u10D0\u10E2\u10E3\u10E0\u10D0: <u class="vue-geokbd--statusMessage-state"></u><br>\n    \u10E9\u10D0\u10E0\u10D7\u10D5\u10D0/\u10D2\u10D0\u10DB\u10DD\u10E0\u10D7\u10D5\u10D0 \u10D9\u10DA\u10D0\u10D5\u10D8\u10E8\u10D8\u10D7 (\'' + options.hotkey + '\')\n  </div>\n';
-	
-	  document.body.appendChild(element);
-	};
-	
-	var syncText = function syncText(enabled) {
-	  var statusMessageStateElement = element.querySelector('.vue-geokbd--statusMessage-state');
-	
-	  statusMessageStateElement.innerHTML = enabled ? STATUS_MSG_TEXT_ON : STATUS_MSG_TEXT_OFF;
-	};
-	
-	var visibility = function visibility(_visibility, enabled, statusMessage) {
-	  if (statusMessage === false) return;
-	  showState = _visibility = !!_visibility;
-	
-	  // Add little debounce to keep consistent visibility state
-	  setTimeout(function () {
-	    if (showState === _visibility) {
-	      element.className = _visibility ? STATUS_MSG_CLASS_VISIBLE : STATUS_MSG_CLASS_HIDDEN;
-	      syncText(enabled);
-	    }
-	  }, 100);
-	};
-	
-	exports.default = { syncText: syncText, visibility: visibility };
+	var TEXT_ENABLED = 'ჩართულია';
+	var TEXT_DISABLED = 'გამორთულია';
+	var CLASSNAME = 'vue-geokbd--statusMessage';
+	var CLASSNAME_STATE = CLASSNAME + '-state';
+	var CLASSNAME_HIDDEN = CLASSNAME + ' hidden';
+	var CLASSNAME_VISIBLE = CLASSNAME + ' visible';
+	var TEMPLATE = ['<div class="vue-geokbd--statusMessage-text">', 'ქართული კლავიატურა: <u class="' + CLASSNAME_STATE + '"></u><br>', 'ჩართვა/გამორთვა კლავიშით (\'{$hotkey}\')', '</div>'].join('');
+	var StatusMsgElement = function () {
+	    function StatusMsgElement() {}
+	    StatusMsgElement.create = function (hotkey) {
+	        this.$root = document.createElement('div');
+	        this.$root.className = CLASSNAME_HIDDEN;
+	        this.$root.innerHTML = TEMPLATE.replace('{$hotkey}', hotkey);
+	        this.$state = this.$root.querySelector('.' + CLASSNAME_STATE);
+	        document.body.appendChild(this.$root);
+	    };
+	    StatusMsgElement.stateText = function (isEnabled) {
+	        this.$state.innerHTML = isEnabled ? TEXT_ENABLED : TEXT_DISABLED;
+	    };
+	    StatusMsgElement.isVisible = function (isVisible) {
+	        this.$root.className = isVisible ? CLASSNAME_VISIBLE : CLASSNAME_HIDDEN;
+	    };
+	    return StatusMsgElement;
+	}();
+	Object.defineProperty(exports, "__esModule", { value: true });
+	exports.default = StatusMsgElement;
 
 /***/ }
 /******/ ])
